53e37e306057736e69bcba5a2c02e93cc4c50890
Syed Zulqarnain Haider <haidershahpak17@gmail.com>
2025-06-21 01:53:10 +0500

    footer temp change

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -5,10 +5,10 @@ import DotLink, { DotDownloadLink } from "../DotLink";
 const Footer = () => {
     
     return (
-        <footer className="container mx-auto py-10 px-5 xl:px-0">
+        <footer className="container mx-auto py-10 px-3 xl:px-0">
             <div className="w-full py-24 border-[1px] border-[#444] flex flex-col justify-center items-center rounded-3xl">
                 <h3 className="text-3xl xl:text-[46px] font-['Mango']">Looking for a new talent?</h3>
-                <h2 className="text-4xl xl:text-[100px] hover:text-[var(--accent-color)] cursor-pointer duration-300 email-link my-5">Haidershahpak17@gmail.com</h2>
+                <h2 className="text-4xl xl:text-[100px] hover:text-[var(--accent-color)] cursor-pointer duration-300 email-link my-5 xl:my-10">Haidershahpak17@gmail.com</h2>
                 <div className="flex gap-10 xl:gap-24">
                     <DotLink href={"https://www.linkedin.com/in/syed-zulqarnain-haider-151655247/"} value={"LinkedIn"} target={"new-tab"} />
                     <DotDownloadLink href={"/docs/Syed-Zulqarnain-Haider-Resume.pdf"} value={"Download CV"} />
